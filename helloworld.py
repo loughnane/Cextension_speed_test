eeaac416292d17cba838ca57bc906c8f4e009926
Chris Loughnane <loughnane.c@gmail.com>
2013-06-23 14:41:17 -0400

    added a sleep between popen and cextension so that the outputs don't get mixed up

--- helloworld.py
+++ helloworld.py
@@ -6,10 +6,12 @@ def hello_cextension():
 
 if __name__ == '__main__':
     import timeit
+    import time
     from subprocess import Popen
     from hello import hello
     number=10
     popen_total_time=(timeit.timeit("hello_Popen()", setup="from __main__ import hello_Popen",number=number))
+    time.sleep(0.1)
     cextension_total_time=(timeit.timeit("hello_cextension()",setup="from __main__ import hello_cextension",number=number))
 
     print "Popen Average: 		{} us per loop".format(popen_total_time/number*1e6)
